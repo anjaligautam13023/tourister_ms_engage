b3332d9e6f9b792772c322a1ad805d5a649c0e71
anjaligautam13023 <anjaligautam13023@gmail.com>
2022-05-29 20:26:09 +0530

    commit changes

--- members/urls.py
+++ members/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    # path('index.html',views.index, name='index'),
-    # path('signup', views.signup, name='signup'),
-    # path('login', views.login, name='login'),
-    # path('page', views.page, name='page'),
-    # path('logout', views.logout, name='logout'),
-    # path('about', views.about, name='about'),
+    path('', views.index, name='index'),
+    path('index.html',views.index, name='index'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('page', views.page, name='page'),
+    path('logout', views.logout, name='logout'),
+    path('about', views.about, name='about'),
 
    
 ]
